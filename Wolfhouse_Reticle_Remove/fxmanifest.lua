ca740afee68c20cf9aa67f02284b1174b40e6cef
Wollffus <58875783+Wollffus@users.noreply.github.com>
2021-12-02 22:03:43 -0800

    init: upload

---
+++ Wolfhouse_Reticle_Remove/fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'cerulean'
+games { 'rdr3', 'gta5' }
+
+author 'https://discord.gg/zGVDppYxfk'
+version '2.1.5'
+
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+this_is_a_map 'yes'
